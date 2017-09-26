6dd65a63306e7fe320c9bb60d66a996a7512c304
Toby Hamand <tobyhamand@gmail.com>
2017-09-26 15:19:29 +0100

    renamed project

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase'
 var config = {
-  apiKey: "AIzaSyAC6bKVQ_TIPeen831ndhrcc9IkFhxUvXk",
-  authDomain: "funmi-s-film-festival.firebaseapp.com",
-  databaseURL: "https://funmi-s-film-festival.firebaseio.com",
-  projectId: "funmi-s-film-festival",
-  storageBucket: "funmi-s-film-festival.appspot.com",
-  messagingSenderId: "243897790581"
+  apiKey: "AIzaSyBLX8USqwOXbvTtgxa5oEA1EAVWtZsv2Tk",
+  authDomain: "funmisfilmfestival.firebaseapp.com",
+  databaseURL: "https://funmisfilmfestival.firebaseio.com",
+  projectId: "funmisfilmfestival",
+  storageBucket: "funmisfilmfestival.appspot.com",
+  messagingSenderId: "270131375245"
 };
 var fire = firebase.initializeApp(config);
 export default fire;
