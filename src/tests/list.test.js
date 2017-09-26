76f0e422a43fbb049610c2d81e0002eaf7edb23a
Toby Hamand <tobyhamand@gmail.com>
2017-09-26 14:58:39 +0100

    removed list test

--- src/tests/list.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import List from '../list';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<List />, div);
-});
