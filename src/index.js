10702482b67dd4751c233312aa39f0f9f0f669c2
Toby Hamand <toby.hamand@triptease.com>
2018-01-30 18:40:08 +0000

    removing service worker to stop caching

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import 'reset-css';
 
 import './index.css';
 import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
